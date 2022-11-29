4b314e6bf4b588743de407cb038771eb980067f6
BenAliasMacc <80431649+BenAliasMacc@users.noreply.github.com>
2022-11-29 13:47:04 +0100

    deploiement

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   useEffect( () => {
     const getToken = async () => {
       try {
-        const res = await axios.get('/jwtid', {
+        const res = await axios.get('http://localhost:5000/jwtid', {
           withCredentials: true
         });        
         setUserId(res.data);           
