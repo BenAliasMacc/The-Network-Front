efc4847457687d7ad9f7e3e2784ae8b3a65dfc88
BenAliasMacc <80431649+BenAliasMacc@users.noreply.github.com>
2022-12-19 20:40:27 +0100

    deploiement

--- src/redux/reducers/postSlice.jsx
+++ src/redux/reducers/postSlice.jsx
@@ -1,7 +1,7 @@
 import { createAsyncThunk, createSlice, createSelector } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const url = "/api/post/"
+const url = "https://social-network-back.vercel.app/api/post/"
 
 export const fetchPost = createAsyncThunk('post/fetchPost', async (num) => {
     try {
